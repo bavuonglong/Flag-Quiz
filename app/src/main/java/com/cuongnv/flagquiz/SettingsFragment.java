17cfff27cd38320c95c5ad23939b9f1b90d004e8
CuongNV <cuong.ngo@axonactive.vn>
2016-11-20 22:31:35 +0700

    - add fragments

---
+++ app/src/main/java/com/cuongnv/flagquiz/SettingsFragment.java
@@ -0,0 +1,10 @@
+package com.cuongnv.flagquiz;
+
+import android.preference.PreferenceFragment;
+
+/**
+ * Created by CuongNV on 11/20/2016.
+ */
+
+public class SettingsFragment extends PreferenceFragment {
+}
